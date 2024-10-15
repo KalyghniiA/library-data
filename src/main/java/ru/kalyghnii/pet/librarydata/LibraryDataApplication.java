7a1df315747579c097d832c3fc1bdebbf1f53594
Александр <kalyghnii@gmail.com>
2024-10-15 15:19:56 +0500

    first_push

---
+++ src/main/java/ru/kalyghnii/pet/librarydata/LibraryDataApplication.java
@@ -0,0 +1,13 @@
+package ru.kalyghnii.pet.librarydata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/kalyghnii/pet/librarydata/LibraryDataApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kalyghnii.pet.librarydata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
